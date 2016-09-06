ab3321a9b070765426c7b9adc7fa963338f08493
anuradha805 <mahanti09@gmail.com>
2016-09-06 12:02:06 +0530

    Update TestClass.java

--- javaproject1/src/mypack1/TestClass.java
+++ javaproject1/src/mypack1/TestClass.java
@@ -3,7 +3,7 @@ package mypack1;
 public class TestClass {
 
 	public static void main(String[] args) {
-		System.out.println("hello test changed");
+		System.out.println("hello test changed second");
 		
 
 	}
