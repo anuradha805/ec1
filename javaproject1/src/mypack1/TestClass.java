d170bbcd5e347eb5cc782e8c9488efbd6a33cbf8
pavansaikota <pavan1062.k@gmail.com>
2016-09-06 12:12:45 +0530

    Changes by pavan
    I am awesome

--- javaproject1/src/mypack1/TestClass.java
+++ javaproject1/src/mypack1/TestClass.java
@@ -4,7 +4,8 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		System.out.println("hello test changed second");
-		
+		System.out.println("I am awesome");
+	//merge it IF pavan is awesome//	
 
 	}
 
